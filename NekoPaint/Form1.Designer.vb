3401f89764c77993235eb8be1a7e288151196b66
m-neko <kazumi@mike-neko.biz>
2024-03-02 18:01:00 +0900

    Add viewer and open file menu

--- NekoPaint/Form1.Designer.vb
+++ NekoPaint/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class Form1
+Partial Class FrmMain
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,10 +22,168 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmMain))
+        MenuStrip1 = New MenuStrip()
+        MenuFile = New ToolStripMenuItem()
+        MenuOpen = New ToolStripMenuItem()
+        MenuSaveAs = New ToolStripMenuItem()
+        ToolStripMenuItem1 = New ToolStripSeparator()
+        MenuExit = New ToolStripMenuItem()
+        StatusStrip1 = New StatusStrip()
+        LblRatio = New ToolStripStatusLabel()
+        LblPictureSize = New ToolStripStatusLabel()
+        PnlTop = New Panel()
+        PnlBottom = New Panel()
+        PnlMain = New Panel()
+        PbxMain = New PictureBox()
+        DlgOpenFile = New OpenFileDialog()
+        MenuStrip1.SuspendLayout()
+        StatusStrip1.SuspendLayout()
+        PnlMain.SuspendLayout()
+        CType(PbxMain, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' MenuStrip1
+        ' 
+        MenuStrip1.Items.AddRange(New ToolStripItem() {MenuFile})
+        MenuStrip1.Location = New Point(0, 0)
+        MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.Size = New Size(800, 24)
+        MenuStrip1.TabIndex = 0
+        MenuStrip1.Text = "MenuStrip1"
+        ' 
+        ' MenuFile
+        ' 
+        MenuFile.DropDownItems.AddRange(New ToolStripItem() {MenuOpen, MenuSaveAs, ToolStripMenuItem1, MenuExit})
+        MenuFile.Name = "MenuFile"
+        MenuFile.Size = New Size(67, 20)
+        MenuFile.Text = "ファイル(&F)"
+        ' 
+        ' MenuOpen
+        ' 
+        MenuOpen.Name = "MenuOpen"
+        MenuOpen.Size = New Size(180, 22)
+        MenuOpen.Text = "ファイルを開く(&O)"
+        ' 
+        ' MenuSaveAs
+        ' 
+        MenuSaveAs.Name = "MenuSaveAs"
+        MenuSaveAs.Size = New Size(180, 22)
+        MenuSaveAs.Text = "名前を付けて保存(&A)"
+        ' 
+        ' ToolStripMenuItem1
+        ' 
+        ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        ToolStripMenuItem1.Size = New Size(177, 6)
+        ' 
+        ' MenuExit
+        ' 
+        MenuExit.Name = "MenuExit"
+        MenuExit.Size = New Size(180, 22)
+        MenuExit.Text = "終了(&X)"
+        ' 
+        ' StatusStrip1
+        ' 
+        StatusStrip1.Items.AddRange(New ToolStripItem() {LblRatio, LblPictureSize})
+        StatusStrip1.Location = New Point(0, 426)
+        StatusStrip1.Name = "StatusStrip1"
+        StatusStrip1.Size = New Size(800, 24)
+        StatusStrip1.TabIndex = 1
+        StatusStrip1.Text = "StatusStrip1"
+        ' 
+        ' LblRatio
+        ' 
+        LblRatio.BorderSides = ToolStripStatusLabelBorderSides.Left Or ToolStripStatusLabelBorderSides.Top Or ToolStripStatusLabelBorderSides.Right Or ToolStripStatusLabelBorderSides.Bottom
+        LblRatio.BorderStyle = Border3DStyle.SunkenOuter
+        LblRatio.Name = "LblRatio"
+        LblRatio.Size = New Size(62, 19)
+        LblRatio.Text = "倍率: x.xx"
+        ' 
+        ' LblPictureSize
+        ' 
+        LblPictureSize.BorderSides = ToolStripStatusLabelBorderSides.Left Or ToolStripStatusLabelBorderSides.Top Or ToolStripStatusLabelBorderSides.Right Or ToolStripStatusLabelBorderSides.Bottom
+        LblPictureSize.BorderStyle = Border3DStyle.SunkenOuter
+        LblPictureSize.Name = "LblPictureSize"
+        LblPictureSize.Size = New Size(153, 19)
+        LblPictureSize.Text = "元の画像サイズ: 1000 × 1000"
+        ' 
+        ' PnlTop
+        ' 
+        PnlTop.Dock = DockStyle.Top
+        PnlTop.Location = New Point(0, 24)
+        PnlTop.Name = "PnlTop"
+        PnlTop.Size = New Size(800, 52)
+        PnlTop.TabIndex = 2
+        ' 
+        ' PnlBottom
+        ' 
+        PnlBottom.Dock = DockStyle.Bottom
+        PnlBottom.Location = New Point(0, 374)
+        PnlBottom.Name = "PnlBottom"
+        PnlBottom.Size = New Size(800, 52)
+        PnlBottom.TabIndex = 3
+        ' 
+        ' PnlMain
+        ' 
+        PnlMain.AutoScroll = True
+        PnlMain.Controls.Add(PbxMain)
+        PnlMain.Dock = DockStyle.Fill
+        PnlMain.Location = New Point(0, 76)
+        PnlMain.Name = "PnlMain"
+        PnlMain.Size = New Size(800, 298)
+        PnlMain.TabIndex = 4
+        ' 
+        ' PbxMain
+        ' 
+        PbxMain.Location = New Point(0, 0)
+        PbxMain.Name = "PbxMain"
+        PbxMain.Size = New Size(4096, 3072)
+        PbxMain.SizeMode = PictureBoxSizeMode.AutoSize
+        PbxMain.TabIndex = 0
+        PbxMain.TabStop = False
+        ' 
+        ' DlgOpenFile
+        ' 
+        DlgOpenFile.Filter = "画像ファイル|*.bmp;*.jpg;*.jpeg;*.png|すべてのファイル|*.*"
+        ' 
+        ' FrmMain
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(PnlMain)
+        Controls.Add(PnlBottom)
+        Controls.Add(PnlTop)
+        Controls.Add(StatusStrip1)
+        Controls.Add(MenuStrip1)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        MainMenuStrip = MenuStrip1
+        Name = "FrmMain"
+        Text = "NekoPaint"
+        MenuStrip1.ResumeLayout(False)
+        MenuStrip1.PerformLayout()
+        StatusStrip1.ResumeLayout(False)
+        StatusStrip1.PerformLayout()
+        PnlMain.ResumeLayout(False)
+        PnlMain.PerformLayout()
+        CType(PbxMain, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents MenuFile As ToolStripMenuItem
+    Friend WithEvents MenuOpen As ToolStripMenuItem
+    Friend WithEvents MenuSaveAs As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem1 As ToolStripSeparator
+    Friend WithEvents MenuExit As ToolStripMenuItem
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents LblRatio As ToolStripStatusLabel
+    Friend WithEvents LblPictureSize As ToolStripStatusLabel
+    Friend WithEvents PnlTop As Panel
+    Friend WithEvents PnlBottom As Panel
+    Friend WithEvents PnlMain As Panel
+    Friend WithEvents PbxMain As PictureBox
+    Friend WithEvents DlgOpenFile As OpenFileDialog
 
 End Class
--- NekoPaint/Form1.vb
+++ NekoPaint/Form1.vb
@@ -1,3 +1,41 @@
-﻿Public Class Form1
+﻿Public Class FrmMain
+
+    Private ratio As Double = 1.0#      ' 画像の拡大率
+    Private bmp As Bitmap = Nothing     ' 画像データ
+
+    Private Sub MenuExit_Click(sender As Object, e As EventArgs) Handles MenuExit.Click
+        Application.Exit()
+    End Sub
+
+    '画面更新処理
+    Private Sub UpdateFormData()
+        LblRatio.Text = "倍率: "
+        LblPictureSize.Text = "元の画像サイズ: "
+        If Not bmp Is Nothing Then
+            LblRatio.Text &= ratio.ToString("0.00")
+            LblPictureSize.Text &= bmp.Width & " × " & bmp.Height
+        Else
+            PbxMain.Size = New Size(0, 0)
+        End If
+    End Sub
+
+    Private Sub FrmMain_Load(sender As Object, e As EventArgs) Handles Me.Load
+        UpdateFormData()
+    End Sub
+
+    Private Sub MenuOpen_Click(sender As Object, e As EventArgs) Handles MenuOpen.Click
+        If DlgOpenFile.ShowDialog() = DialogResult.OK Then
+            Try
+                bmp = New Bitmap(DlgOpenFile.FileName)
+                PbxMain.Image = bmp
+                ratio = 1.0#
+            Catch ex As Exception
+                bmp = Nothing
+                PbxMain.Image = Nothing
+                MessageBox.Show("画像の読み込みに失敗しました", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+            UpdateFormData()
+        End If
+    End Sub
 
 End Class
